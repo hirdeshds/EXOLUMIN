81d1a3add5ef0693e0a764ee2f31885aaf0c6f3a
Hirdesh <hirdeshhkumar2005@gmail.com>
2025-10-05 00:44:59 +0530

    update

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import joblib
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = joblib.load("models/Random_forest_exo.pkl")
 scaler = joblib.load("models/scaler.pkl")
@@ -31,12 +40,14 @@ class InputData(BaseModel):
 @app.post("/predict")
 def predict(data: InputData):
     input_dict = data.dict()
+    
     missing_cols = [col for col in columns if col not in input_dict]
     if missing_cols:
         raise HTTPException(status_code=400, detail=f"Missing feature(s): {missing_cols}")
     
     features = np.array([input_dict[col] for col in columns]).reshape(1, -1)
     features_scaled = scaler.transform(features)
+    
     pred = model.predict(features_scaled)[0]
     pred_proba = model.predict_proba(features_scaled)[0][1]
     
